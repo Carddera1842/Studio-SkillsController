a7e64533b73ef34af75fc52bf9e8697d8e903f1b
Carddera1842 <carddera1842@gmail.com>
2024-10-07 15:48:21 -0500

    "Thymeleaf Chapter Example"

---
+++ Java/practice-thymeleaf/practice-thymeleaf/src/main/java/org/launchcode/practice_thymeleaf/HelloSpringController.java
@@ -0,0 +1,42 @@
+package org.launchcode.practice_thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@Controller
+public class HelloSpringController {
+
+    //responds to /hello?name=LaunchCode
+    @RequestMapping(value= "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String helloAgain(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
+    }
+
+    @GetMapping("form")
+    public String helloForm() {
+        return "form";
+
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+
+}
